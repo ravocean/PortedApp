4db14de0564a20b04b14ed05391d1b6928c04f39
ravocean <iamericanec@gmail.com>
2021-10-14 18:02:16 -0700

    Add project files.

---
+++ PortedApp/Pets.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortedApp
+{
+    class Pets
+    {
+
+        private string name;
+        private string sound;
+
+        public const string SHELTER = "Animal Shelter";
+
+        public readonly int idNum;
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+
+        // Define constructors
+        public Pets()
+            : this("No Name", "No Sound") { }
+
+        public Pets(string name)
+            : this(name, "No Sound") { }
+
+        public Pets(string name, string sound)
+        {
+            SetName(name);
+            Sound = sound;
+
+            NumOfPets = 1;
+
+            Random random = new Random();
+            idNum = random.Next(1, 10000);
+        }
+
+        // Define setters
+        public void SetName(string name)
+        {
+            if (!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            } else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name cannot contain numbers");
+            }
+        }
+
+        // Define getter
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Properties
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if(value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");
+                }
+                sound = value;
+            }
+        }
+
+        public string PetOwner { get; set; } = "No Pet Owner";
+
+        public static int numOfPets = 0;
+
+        public static int NumOfPets
+        {
+            get { return numOfPets; }
+            set { numOfPets += value; }
+        }
+
+    } // end of Pets class
+}
---
+++ PortedApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PortedApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pets dog = new Pets();
+
+            dog.SetName("Olive");
+            dog.Sound = "Woof!";
+
+            Console.WriteLine("The dog's name is {0} and {0} says {1}",
+                dog.GetName(), dog.Sound);
+
+            // Check auto-generated getters and setters
+            dog.PetOwner = "Zack";
+
+            Console.WriteLine("{0}'s owner is {1}",
+                dog.GetName(), dog.PetOwner);
+
+            Console.WriteLine("{0}'s shelter ID is {1}",
+                dog.GetName(), dog.idNum);
+
+            // Create another animal to check the NumOfPets
+            Pets monkey = new Pets("Marcel", "Ooh-ooh, ah-ah!");
+
+            // Check the number of pets
+            Console.WriteLine("Number of pets: {0}",
+                Pets.NumOfPets);
+
+            Console.ReadLine();
+        }
+    }
+}
